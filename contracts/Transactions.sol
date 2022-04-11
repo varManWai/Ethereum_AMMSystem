f741f064b557e6673b9cb5cf45e16fb901e4acac
Ho Wai Kit <68110992+1HoWK@users.noreply.github.com>
2022-04-11 18:52:48 +0800

    Hello

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -2,9 +2,6 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Transactions {
-    // uint x = 0;
-    // uint y = 0;
-    // uint constant k = x * y;
 
     // event Transfer (address sender, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
