b0a7b05c2c5bb1ac9783cfccca122b8bf60fd951
Ho Wai Kit <68110992+1HoWK@users.noreply.github.com>
2022-04-11 18:52:28 +0800

    Added liquidity pool

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -1,7 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Uniswap{
+contract Transactions {
+    // uint x = 0;
+    // uint y = 0;
+    // uint constant k = x * y;
+
+    // event Transfer (address sender, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+    // function publishTransaction(address payable receiver, uint amount, string memory message, string memory keyword) public {
+    //     emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword); 
+    // }
+
+    function calculationXYK(address payable receiver, uint amount) public {
+
+    }
+
+    function payTransctions() public {
+
+    }
+
     
 
 }
